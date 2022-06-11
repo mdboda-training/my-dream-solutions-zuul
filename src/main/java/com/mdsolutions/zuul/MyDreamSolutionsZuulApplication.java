75a8e27365d46bc189369f154d6b4f67cc39b348
MuraliDhar Boda <100523854+mdboda-training@users.noreply.github.com>
2022-06-11 13:17:29 +0530

    Create MyDreamSolutionsZuulApplication.java

---
+++ src/main/java/com/mdsolutions/zuul/MyDreamSolutionsZuulApplication.java
@@ -0,0 +1,13 @@
+package com.mdsolutions.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDreamSolutionsZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDreamSolutionsZuulApplication.class, args);
+	}
+
+}
