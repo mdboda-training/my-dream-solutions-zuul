55ee8339a8f7a803486b9c8d3af22c6d770d2b08
MuraliDhar Boda <100523854+mdboda-training@users.noreply.github.com>
2022-06-11 13:20:11 +0530

    Create MyDreamSolutionsZuulApplicationTests.java

---
+++ src/test/java/com/mdsolutions/zuul/MyDreamSolutionsZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsolutions.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDreamSolutionsZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
